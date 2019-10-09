5923b3d496fd4ba5755da14553cc76c0a8cbd320
G'd(AM)légoss <37986499+G'd(AM)lgoss@users.noreply.github.com>
2019-10-09 20:16:27 +0200

    Finition admin

--- main/admin.py
+++ main/admin.py
@@ -2,13 +2,32 @@ from django.contrib import admin
 from .models import drinks,rooms,history,stocks,food
 
 class stocksAdmin(admin.ModelAdmin):
-    list_display   = ('drink', 'room', 'quantity')
+    list_display   = ('drink', 'room', 'quantity',)
     list_filter    = ('room', 'drink',)
     ordering       = ('room', 'drink',)
 
+class historyAdmin(admin.ModelAdmin):
+    list_display   = ('drink', 'room', 'quantity',)
+    list_filter    = ('room', 'drink',)
+    date_hierarchy = 'date'
+    ordering       = ('date',)
 
-admin.site.register(drinks)
-admin.site.register(rooms)
-admin.site.register(history)
+class drinksAdmin(admin.ModelAdmin):
+    list_display   = ('name', 'container_size', 'threshold','by_bottle','is_champagne',)
+    list_filter    = ('name','is_champagne',)
+    ordering       = ('name',)
+
+class roomsAdmin(admin.ModelAdmin):
+    list_display   = ('name', 'is_bar',)
+    list_filter    = ('name','is_bar',)
+    ordering       = ('name',)
+
+class foodAdmin(admin.ModelAdmin):
+    list_display   = ('name',)
+    ordering       = ('name',)
+
+admin.site.register(drinks, drinksAdmin)
+admin.site.register(rooms, roomsAdmin)
+admin.site.register(history, historyAdmin)
 admin.site.register(stocks, stocksAdmin)
-admin.site.register(food)
+admin.site.register(food, foodAdmin)
