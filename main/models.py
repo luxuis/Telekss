df1ce9a4fda934ef1d9f399fa1561a66496c8f6d
G'd(AM)légoss <37986499+G'd(AM)lgoss@users.noreply.github.com>
2019-10-09 17:22:23 +0200

    Création de la BDD

--- main/models.py
+++ main/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class food(models.Model):
+    name = models.CharField(max_length = 100)
+
+    class Meta:
+        verbose_name = "food"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class drinks(models.Model):
+    name = models.CharField(max_length = 50)
+    container_size = models.IntegerField()
+    threshold = models.IntegerField()
+    by_bottle = models.IntegerField()
+    is_champagne = models.BooleanField()
+
+    class Meta:
+        verbose_name = "drinks"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class rooms(models.Model):
+    name = models.CharField(max_length = 30)
+    is_bar = models.BooleanField()
+    drinks = models.ManyToManyField(drinks, through = 'stocks')
+
+    class Meta:
+        verbose_name = "rooms"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class stocks(models.Model):
+    drink = models.ForeignKey(drinks, on_delete = models.CASCADE)
+    room = models.ForeignKey(rooms, on_delete = models.CASCADE)
+    quantity = models.IntegerField()
+    consommation = models.IntegerField()
+
+    class Meta:
+        verbose_name = "rooms"
+        ordering = ['room','drink']
+
+    def __str__(self):
+        return str(self.room)+str(self.drink)
+
+class history(models.Model):
+    drink = models.CharField(max_length = 50)
+    room = models.CharField(max_length = 30)
+    date = models.DateTimeField(default=timezone.now)
+    quantity = models.IntegerField()
+    is_sale = models.BooleanField()
+    is_cancelled = models.BooleanField()
